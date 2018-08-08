44030856e20dfe3b3648dd0b7ad68760321c5420
Ben Zhao <kepingz@microsoft.com>
2018-08-08 00:42:05 -0700

    Add event handler to fab to start react activity.

--- android/app/src/main/java/com/example/ben/onedrivereact/MainActivity.java
+++ android/app/src/main/java/com/example/ben/onedrivereact/MainActivity.java
@@ -29,8 +29,8 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, MyReactActivity.class);
+                MainActivity.this.startActivity(intent);
             }
         });
 

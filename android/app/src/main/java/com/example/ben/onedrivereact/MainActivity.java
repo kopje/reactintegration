ce46ee789c057aac9b9f8ea3a6dd3d8cd4b28668
Ben Zhao <kepingz@microsoft.com>
2018-08-07 23:56:36 -0700

    Configure permissions for development error overlay

--- android/app/src/main/java/com/example/ben/onedrivereact/MainActivity.java
+++ android/app/src/main/java/com/example/ben/onedrivereact/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.ben.onedrivereact;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    private final String TAG = MainActivity.class.getCanonicalName();
+    private final int OVERLAY_PERMISSION_REQ_CODE = 11111;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +33,15 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        // Ask for permission of ACTION_MANAGE_OVERLAY_PERMISSION for showing React Native dev errors.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.canDrawOverlays(this)) {
+                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                        Uri.parse("package:" + getPackageName()));
+                startActivityForResult(intent, OVERLAY_PERMISSION_REQ_CODE);
+            }
+        }
     }
 
     @Override
@@ -48,5 +64,16 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == OVERLAY_PERMISSION_REQ_CODE) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (!Settings.canDrawOverlays(this)) {
+                    Log.e(TAG, "onActivityResult: ", null);
+                }
+            }
+        }
     }
 }
